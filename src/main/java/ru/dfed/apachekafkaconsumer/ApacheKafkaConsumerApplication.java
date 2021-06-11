09919e7a99e8ac0b148b4acbd3b9d8e3165fa05d
Dmitriy_Fedotov <Dmitriy_Fedotov@epam.com>
2021-06-11 14:54:25 +0300

    Initial commit

---
+++ src/main/java/ru/dfed/apachekafkaconsumer/ApacheKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package ru.dfed.apachekafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheKafkaConsumerApplication.class, args);
+	}
+
+}
